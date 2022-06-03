b7e637fe553b1e9a538a7d5005346695a62bd223
dobbibobbi <80916956+dobbibobbi@users.noreply.github.com>
2022-06-03 15:42:32 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+
+import { Telegraf } from 'telegraf'
+import dotenv from 'dotenv'
+dotenv.config()
+const {BOT_TOKEN} = process.env;
+import CoinGecko from 'coingecko-api'
+
+
+const bot = new Telegraf(BOT_TOKEN)
+
+
+async function getCoinPrice(coinSymbol){
+    const CoinGeckoClient = new CoinGecko();
+    const data = await CoinGeckoClient.coins.list()
+    const coinList = data['data']
+    for (let coin of coinList){
+        if ( coin.symbol === coinSymbol ){
+            console.log(coin)
+            const prices = await CoinGeckoClient.coins.fetchMarketChart(coin.id);
+            const coinPrice = Number(prices.data.prices[0][1].toFixed(2))
+            return coinPrice
+            break
+        }
+    }
+}
+
+bot.on('text', async ctx => {
+    const coinSymbol = ctx.message.text.toLowerCase()
+    let coinPrice = await getCoinPrice(coinSymbol)
+    if ( coinPrice != undefined){
+        ctx.reply(`${ctx.message.text.toLowerCase()}: ${coinPrice}`)
+    }
+})
+
+bot.launch()
