6e0aa7f729d68967c12ac930414f004d843974eb
dobbibobbi <80916956+dobbibobbi@users.noreply.github.com>
2022-06-03 19:12:45 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 import { Telegraf } from 'telegraf'
 import dotenv from 'dotenv'
 dotenv.config()
@@ -7,22 +6,11 @@ import CoinGecko from 'coingecko-api'
 
 
 const bot = new Telegraf(BOT_TOKEN)
+const CoinGeckoClient = new CoinGecko();
+
 const coinListPushka = ['bitcoin', 'ethereum', 'polkadot', 'acala', 'moonbeam', 'pancakeswap-token']
 
-async function getCoinIdPrice(coinId){
-    const CoinGeckoClient = new CoinGecko();
-    const data = await CoinGeckoClient.coins.list()
-    const coinList = data['data']
-    for (let coin of coinList){
-        if ( coin.id === coinId ){
-            const prices = await CoinGeckoClient.coins.fetchMarketChart(coin.id);
-            const coinPrice = Number(prices.data.prices[0][1].toFixed(2))
-            return coinPrice
-            break
-        }
-    }
-}
-
+// Получаем курс монеты по ее Symbol
 async function getCoinSymbolPrice(coinSymbol){
     const CoinGeckoClient = new CoinGecko();
     const data = await CoinGeckoClient.coins.list()
@@ -37,14 +25,45 @@ async function getCoinSymbolPrice(coinSymbol){
     }
 }
 
+// Получаем курс монеты по ее ID
+async function getCoinIdPrice(coinId){
+    const CoinGeckoClient = new CoinGecko();
+    const data = await CoinGeckoClient.coins.list()
+    const coinList = data['data']
+    for (let coin of coinList){
+        if ( coin.id === coinId ){
+            const prices = await CoinGeckoClient.coins.fetchMarketChart(coin.id);
+            const coinPrice = Number(prices.data.prices[0][1].toFixed(2))
+            return coinPrice
+            break
+        }
+    }
+}
+
+const data = await CoinGeckoClient.coins.list()
+const coinList = data['data']
+const coinIdList = []
+const coinSymbolList = []
+// создаем списки из ID и Symbol каждой монеты
+for (let coin of coinList){
+    coinIdList.push(coin.id)
+    coinSymbolList.push(coin.symbol)
+}
+
+
 bot.on('text', async ctx => {
     const coinSymbol = ctx.message.text.toLowerCase()
-    let coinPrice = await getCoinSymbolPrice(coinSymbol)
-    if ( coinPrice != undefined){
-        ctx.reply(`${ctx.message.text.toLowerCase()}: ${coinPrice}$`)
+    //проверяем есть ли такой Symbol
+    if (coinSymbolList.indexOf(coinSymbol) >= 0){
+        let coinPrice = await getCoinSymbolPrice(coinSymbol)
+        // получаем курс монет
+        ctx.reply(`${ctx.message.text.toLowerCase()}: ${coinPrice}$`) 
         setTimeout(() => ctx.deleteMessage(ctx.message.message_id), 10000)
         setTimeout(() => ctx.deleteMessage(ctx.message.message_id+1), 10000)
     }
+
+
+    // реакция на команду пушка
     if (ctx.message.text.toLowerCase() === 'пушка' || ctx.message.text.toLowerCase() === 'geirf'){
         let text = ''
         for (let coinId of coinListPushka){
