ad5062796b0c21bc5fcbc70eb1da531f4b9eccc0
dobbibobbi <80916956+dobbibobbi@users.noreply.github.com>
2022-06-03 17:20:55 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,15 +7,28 @@ import CoinGecko from 'coingecko-api'
 
 
 const bot = new Telegraf(BOT_TOKEN)
+const coinListPushka = ['bitcoin', 'ethereum', 'polkadot', 'acala', 'moonbeam', 'pancakeswap-token']
 
+async function getCoinIdPrice(coinId){
+    const CoinGeckoClient = new CoinGecko();
+    const data = await CoinGeckoClient.coins.list()
+    const coinList = data['data']
+    for (let coin of coinList){
+        if ( coin.id === coinId ){
+            const prices = await CoinGeckoClient.coins.fetchMarketChart(coin.id);
+            const coinPrice = Number(prices.data.prices[0][1].toFixed(2))
+            return coinPrice
+            break
+        }
+    }
+}
 
-async function getCoinPrice(coinSymbol){
+async function getCoinSymbolPrice(coinSymbol){
     const CoinGeckoClient = new CoinGecko();
     const data = await CoinGeckoClient.coins.list()
     const coinList = data['data']
     for (let coin of coinList){
         if ( coin.symbol === coinSymbol ){
-            console.log(coin)
             const prices = await CoinGeckoClient.coins.fetchMarketChart(coin.id);
             const coinPrice = Number(prices.data.prices[0][1].toFixed(2))
             return coinPrice
@@ -26,11 +39,21 @@ async function getCoinPrice(coinSymbol){
 
 bot.on('text', async ctx => {
     const coinSymbol = ctx.message.text.toLowerCase()
-    let coinPrice = await getCoinPrice(coinSymbol)
+    let coinPrice = await getCoinSymbolPrice(coinSymbol)
     if ( coinPrice != undefined){
-        ctx.reply(`${ctx.message.text.toLowerCase()}: ${coinPrice}`)
+        ctx.reply(`${ctx.message.text.toLowerCase()}: ${coinPrice}$`)
         setTimeout(() => ctx.deleteMessage(ctx.message.message_id), 10000)
         setTimeout(() => ctx.deleteMessage(ctx.message.message_id+1), 10000)
+    }
+    if (ctx.message.text.toLowerCase() === 'пушка' || ctx.message.text.toLowerCase() === 'geirf'){
+        let text = ''
+        for (let coinId of coinListPushka){
+            let price = String(await getCoinIdPrice(coinId))
+            text = text + `${coinId}: ${price}$\n`
+        }
+        ctx.reply(text)
+        setTimeout(() => ctx.deleteMessage(ctx.message.message_id), 20000)
+        setTimeout(() => ctx.deleteMessage(ctx.message.message_id+1), 20000)
     }
 })
 
